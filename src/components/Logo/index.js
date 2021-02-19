fadc2607f3bb4eb38f179bc18797ed3a2df4108c
Kristina <kramlj00@fesb.hr>
2021-02-19 12:34:17 +0100

    Adding Logo

---
+++ src/components/Logo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const Logo = () => (
+  <section className={styles.logo}>
+    <span className={styles.logoText}>Andrijić House</span>
+    <span className={styles.sub}>KORČULA-CROATIA</span>
+  </section>
+)
+
+export default Logo
