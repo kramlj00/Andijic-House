35788ab9fe431f92066f0dab9dd21a50010bd89d
Kristina <kramlj00@fesb.hr>
2021-02-19 14:05:40 +0100

    Adding Carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 
 import NavigationHeader from '../components/NavigationHeader'
+import Carousel from '../components/Carousel'
 import Footer from '../components/Footer'
 
 const IndexPage = () => (
   <main>
     <NavigationHeader activeTab = "Home"/>
+    <Carousel/>
     <Footer/>
   </main>
 )
