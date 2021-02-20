af51aceeff37dac0c41e6ffa0f83fa68a623bae5
Kristina <kramlj00@fesb.hr>
2021-02-20 14:54:53 +0100

    Creating Combobox: Look and style

--- src/constants/const.js
+++ src/constants/const.js
@@ -4,3 +4,21 @@ export const navs = [
     {tab: 'Exterior', to: '/exterior'}, 
     {tab: 'Contact', to: '/contact'}
 ]
+
+export const languages = [
+    {
+        language: "English",
+        name: "eng",
+        flag: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png"
+    },
+    {
+        language: "Italiano",
+        name: "it",
+        flag: "https://cdn11.bigcommerce.com/s-ey7tq/images/stencil/1280x1280/products/3332/18818/italy-flag__19720.1575335174.jpg?c=2"
+    },
+    {
+        language: "Deutsche",
+        name: "de",
+        flag: "https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg"
+    },
+]
