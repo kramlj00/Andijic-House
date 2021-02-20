26bd86feb972be557b058751a61b343b2e5def5b
Kristina <kramlj00@fesb.hr>
2021-02-20 14:32:29 +0100

    Removing error in Location

--- src/components/Location/index.js
+++ src/components/Location/index.js
@@ -11,7 +11,7 @@ const Location = () => (
             <h2 className={styles.locationTitle}>Location</h2>
         </Bounce> 
         <div>
-        <iframe className={styles.map} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11686.976429150827!2d16.715967!3d42.9204375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134a720c5f6330cf%3A0x810796f65a20ec98!2sAndriji%C4%87%20House!5e0!3m2!1shr!2shr!4v1613756560386!5m2!1shr!2shr" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" ></iframe>
+        <iframe className={styles.map} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11686.976429150827!2d16.715967!3d42.9204375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134a720c5f6330cf%3A0x810796f65a20ec98!2sAndriji%C4%87%20House!5e0!3m2!1shr!2shr!4v1613756560386!5m2!1shr!2shr" frameborder="0" allowfullscreen="" aria-hidden="false" title="Map" ></iframe>
         </div>
         <div className={styles.location}>
         <Slide triggerOnce={true} delay={300}>
