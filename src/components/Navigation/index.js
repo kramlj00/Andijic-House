3504a7ca66b74b5c21a220847d7c17f7987fdbcd
Kristina <kramlj00@fesb.hr>
2021-02-20 16:13:13 +0100

    Creating Combobox: Internal logic

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
-
+import ComboBox from '../ComboBox'
+ 
 import NavigationBar from '../NavigationBar'
-import Combobox from '../Combobox'
 import styles from './style.module.css'
-
+ 
 const Navigation = ({ activeTab }) => (
     <section className={styles.navigation}>
-        <NavigationBar activeTab={activeTab}/>
-        <Combobox />
+        <NavigationBar activeTab={activeTab} />
+         <ComboBox/>
     </section>
-  )
-
+)
+ 
 export default Navigation
