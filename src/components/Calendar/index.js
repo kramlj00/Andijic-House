267b2f113232fe2f1a6c1d7ae217e39f68ee9447
Kristina <kramlj00@fesb.hr>
2021-03-13 13:13:02 +0100

    Adding effects to calendar

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,10 +1,17 @@
 import React from "react"
 import styles from './style.module.css'
 
+import { Bounce } from "react-awesome-reveal";
+import { Slide } from "react-awesome-reveal";
+
 const Calendar = () => (
     <nav align="center">
-        <h2 className={styles.calendarTitle}>Calendar</h2>
-        <iframe className={styles.calendar} src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FBelgrade&amp;src=YW5kcmlqaWNob3VzZUBnbWFpbC5jb20&amp;color=%23D50000&amp;showNav=1&amp;showTitle=0&amp;showTabs=1&amp;showTz=0&amp;showCalendars=0&amp;hl=en" frameborder="0" scrolling="no"></iframe>
+        <Bounce triggerOnce={true}>
+            <h2 className={styles.calendarTitle}>Calendar</h2>
+        </Bounce>
+        <Slide triggerOnce={true}>
+            <iframe className={styles.calendar} src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FBelgrade&amp;src=YW5kcmlqaWNob3VzZUBnbWFpbC5jb20&amp;color=%23D50000&amp;showNav=1&amp;showTitle=0&amp;showTabs=1&amp;showTz=0&amp;showCalendars=0&amp;hl=en" frameborder="0" scrolling="no"></iframe>
+        </Slide>
     </nav>
 )
  
